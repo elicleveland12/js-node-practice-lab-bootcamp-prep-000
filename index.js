ddd51395d1851257df20a58a1571375cdbfc6ac3
Eli Cleveland <elicleveland12@gmail.com>
2018-01-12 02:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Eli"
-var height = "72"
+var height = 72
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
 
